ff721b1bcbc84c55799bd7ac27afcba262379ab7
Natasha Thapliyal <natashathapliyal@Natashas-MacBook-Air.local>
2015-07-24 19:32:05 -0700

    initial

---
+++ ecommerce/js/ecommerce.js
@@ -0,0 +1,80 @@
+var moleskineOne = {
+	name: "Moleskine Ruled Notebook",
+	category: "notebook",
+	price: 16.95,
+	description: "Be snooty and hipster with a classic Moleskine. But go easy on the crumpling this shit's expensive!",
+	scale: 10,
+	picture_url: "../images/moleskine.png"
+}
+
+var compOne = {
+	name: "Mead Composition College-Ruled Notebook",
+	category: "notebook",
+	price: 3.48,
+	description: "Can't go wrong with a classic comp book. Includes cheap but perfectly thin paper for your crumpling needs",
+	scale: 7,
+	picture_url: "../images/comp.png"
+}
+
+var sketchOne = {
+	name: "Strathmore Sketcbook",
+	category: "wotebook",
+	price: 10.99,
+	description: "Feeling sketchy? Not ideal for crumpling but I mean, why would you crumple sketches? Keep 'em pristine!",
+	scale: 0,
+	picture_url: "../images/strathmoresketch.png"
+}
+
+var looseOne = {
+	name: "Loose Notebook Paper",
+	category: "paper",
+	price: 5.68,
+	description: "Just a packet of loose ruled paper if ya don't feel like being 'bound'",
+	scale: 8,
+	picture_url: "../images/paper.png"
+}
+
+var micronSingle = {
+	name: "Micron Single",
+	category: "writing",
+	price: 3.62,
+	description: "THE BEST PEN TO WRITE/DRAW WITH HANDS DOWN",
+	picture_url: "../images/micron.jpeg"
+}
+
+var micronPackOne = {
+	name: "Micron 12-Pack",
+	category: "writing",
+	price: 9.25,
+	description: "THE BEST PEN TO WRITE/DRAW WITH HANDS DOWN NOW IN PACK OF 12!!!! Grab em quick",
+	picture_url: "../images/micron2.gif"
+}
+
+var bicBlue = {
+	name: "Bic Cristal Ballpoint Pen (BLUE) 12 Pack",
+	category: "writing",
+	price: 3.29,
+	description: "These classic Bic pens are cheap AND write well. Perfecto fer writin' on dat crumbled paper",
+	picture_url: "../images/bicblue.jpg"
+}
+
+var bicPackOne = {
+	name: "BIC 4-Color Ballpoint Pen 3 Pack",
+	category: "writing",
+	price: 3.83,
+	description: "If you want to be cool and French use these pens",
+	picture_url: "../images/bic_three_pack.jpg"
+}
+
+var brace = {
+	name: "Wrist Brace",
+	category: "body care",
+	price: 12.22,
+	description: "Trust me, you're going to need this wrist brace with all those pages you've been crumbling",
+	picture_url: "../images/wristbrace.png"
+}
+
+$('#moleskine1 .name').append('<p>' + moleskineOne.name + '</p>')
+
+
+
